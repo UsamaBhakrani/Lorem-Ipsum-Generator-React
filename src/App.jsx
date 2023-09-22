1024c5221f773417c8f5898baadb718e35a106cf
Mohammed Usama <usamabhakrani@gmail.com>
2023-09-22 18:50:40 +0500

    App Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
+import { useState } from "react";
+import data from "./data";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
 
-export default App
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section className="section-center">
+      <h3>Tired of boring Lorem Ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">Paragraphs:</label>
+        <input
+          value={count}
+          type="number"
+          id="amount"
+          name="amount"
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          generate
+        </button>
+      </form>
+      
+    </section>
+  );
+};
+
+export default App;
